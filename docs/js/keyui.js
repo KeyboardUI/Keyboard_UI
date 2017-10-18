50efd28d80e0d56a12a995bc9ae0d9f9b62c9b2e
wisniewski94 <lion7942@gmail.com>
2017-10-19 01:20:57 +0200

    transparent inputs

--- docs/js/keyui.js
+++ docs/js/keyui.js
@@ -1,9 +1,18 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
     $("body").on('click', ".drp-u li", function() {
         var txt = $(this).text();
         $(this).parent().prev(".drp-txt").text(txt);
         $(this).parent().prevAll('.drp-i').val(txt);
     });
-});
+});*/
+document.addEventListener('mouseup', function(e) {
+    if (e.target.classList.contains('ui') && e.target.localName == 'button') {
+        e.target.blur();
+    }
+}, false);
 
-
+document.addEventListener('click', function(e) {
+    if (e.target.classList.contains('ui') && e.target.localName == 'button') {
+        console.log(e.target.localName)
+    }
+}, false);
--- src/keyui.js
+++ src/keyui.js
@@ -1,9 +1,11 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
     $("body").on('click', ".drp-u li", function() {
         var txt = $(this).text();
         $(this).parent().prev(".drp-txt").text(txt);
         $(this).parent().prevAll('.drp-i').val(txt);
     });
+});*/
+var el = document.querySelector("button.ui");
+el.addEventListener("mousedown", function() {
+    this.blur();
 });
-
-
