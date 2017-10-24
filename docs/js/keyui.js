ca892ebdfc9019973c59c5bf24e69247ce7cff83
wisniewski94 <lion7942@gmail.com>
2017-10-24 02:35:58 +0200

    focus on tab feature

--- docs/js/keyui.js
+++ docs/js/keyui.js
@@ -5,28 +5,13 @@
         $(this).parent().prevAll('.drp-i').val(txt);
     });
 });*/
-/*document.addEventListener('mouseup', function(e) {
-    if (e.target.classList.contains('ui') && e.target.localName == 'button') {
+
+/* This is focus script. 
+ * Disables outlines on click.
+*/
+
+document.addEventListener('mouseup', function(e) {
+    if (e.target.tagName == 'BUTTON') {
         e.target.blur();
     }
 }, false);
-
-document.addEventListener('click', function(e) {
-    if (e.target.classList.contains('ui') && e.target.localName == 'button') {
-        console.log(e.target.localName)
-    }
-}, false);*/
-var focus = document.querySelectorAll('.focus');
-console.log(focus);
-document.addEventListener('keydown', focusTab, false)
-
-for (var i =0; i<focus.length; i++) {
-    focus[i].classList.toggle('focus');
-    focus[i].classList.add('outoffocus');
-}
-
-function focusTab(e) {
-    if(e.target.classList.contains('outoffocus') && e.keyCode === 9) {
-        e.target.classList.add('focus');
-    }
-}
